c6db2ee3ebd78b2a55240551646f3d21ca6e5772
Vutivi Baloyi <130641972+Vuts06@users.noreply.github.com>
2024-09-13 11:51:13 +0200

    Add files via upload
    Excited to share a new milestone in my development journey! 🚀 I recently completed a C# prototype for a Contract Monthly Claim System (CMCS). This system is designed to streamline the process of submitting and approving monthly claims for Independent Contractor lecturers, providing a user-friendly interface and improving administrative workflows.

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace CMCS
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TabControl_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
